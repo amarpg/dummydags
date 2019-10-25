724c1b91a5c58941a9c5c161b81d4bddaa82d8f9
amarpg <56983617+amarpg@users.noreply.github.com>
2019-10-24 22:34:16 -0400

    Update HelloWorld7.py
    merge

--- HelloWorld7.py
+++ HelloWorld7.py
@@ -15,7 +15,7 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-dag = DAG('HelloWorld6', default_args=default_args)
+dag = DAG('HelloWorld7', default_args=default_args)
 
 # t1, t2, t3 and t4 are examples of tasks created using operators
 
